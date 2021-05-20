db2b0e47e1bd0c3eff7f09e98c193f41e92f571d
fangzhi <tongren@startdt.com>
2021-05-20 17:07:52 +0800

    vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,29 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <a-button>antd</a-button>
-      <NuxtLink to="/about">
-        About (internal link that belongs to the Nuxt App)
-      </NuxtLink>
-    </div>
-  </div>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6">
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-btn @click="handleChange">theme{{ $vuetify.theme.dark }}</v-btn>
+    </v-col>
+  </v-row>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
+import Logo from '~/components/Logo.vue'
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
 
-export default Vue.extend({})
-</script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+export default {
+  components: {
+    Logo,
+    VuetifyLogo,
+  },
+  methods: {
+    handleChange() {
+      console.log(this)
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark
+    },
+  },
 }
-</style>
+</script>
